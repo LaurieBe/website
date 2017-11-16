911ccd1c68c796745f41958ab55125648b7db643
Lilou <laurie.besinet@gmail.com>
2017-11-16 15:44:35 +1300

    traduction + couleurs theme

---
+++ laurie.besinet.net/_include/js/trad_i18next_cv.js
@@ -0,0 +1,138 @@
+
+  i18next.init({
+    lng: 'en',
+    debug: true,
+    resources: {
+      en: {
+        translation: {
+          "key_title": "Laurie Bésinet",
+          "key_welcome": "Welcome",
+          "key_hello": "Actually agronomist in a startup offering solutions with connected sensors and agronomic models for farmers.<br> I'm looking for new challenges, that's why I leave soon to discover a country, its peoples, its cultures and its agricultures : New-Zealand",
+          "key_newzealand": "New-Zealand",
+          "key_resume": "Resume",
+          "key_experiences": "Experiences",
+          "key_projects": "Projects",
+          "key_contact": "Contact",
+          "key_home": "Home",
+
+          "key_job_wnt_entr": "Weenat",
+          "key_job_wnt_desc": "<a target=\"_blank\" href=\"http://www.weenat.com\">www.weenat.com</a> | 5 to 15 employees | Nantes, France | Weenat allows the farmers to optimize their resources, by bringing them localized information in the plot to support their decisions.",
+          "key_job_wnt_date1": "Jan 2016 - Nov 2017",
+          "key_job_wnt_title1": "Projects Manager",
+          "key_job_wnt_text1": "Four main missions:",
+          "key_job_wnt_mission1": "Create, developp and improve agronomic models and Decision Support Tools (DST)<span class=\"font-icon-plus\"></span><span class=\"font-icon-minus\"></span>",
+          "key_job_wnt_proj1": "My main mission was to supervise creation and improvement of agronomic models and Decision Support Tools (DST) based on data (rain, humidity, temperature, soil tensiometry and temperature, ...) from sensors into the fields. The aim was to give farmers or researchers tools to confirm their decisions. As a Product Manager, I managed these projects from A to Z through: <br> - identifying users' needs and expectations <br> - structuring the model or calculations and data analyses <br> - leading the different trades involved (electronics, development) or teams from other companies <br> - implementing tests to confirm the interest of the DSTs <br> - placing the product on the market with commercial team. <br>Ten DST projects were successfully completed on various topics (irrigation, pests, fertilization, crop protection products, monitoring of growth and development).",
+          "key_job_wnt_mission2": "Research and development of new strategic partnerships <span class=\"font-icon-plus\"></span><span class=\"font-icon-minus\"></span>",
+          "key_job_wnt_proj2": "I've participated to congresses and colloquies to meet people of the agricultural sector who could work with Weenat on innovation projects. Two innovative projects I contributed to launch: <br> - Detecting and manage nitrogen doses in the field thanks to new sensors and models <br>&nbsp;&nbsp;&nbsp;o Weenat’s selected project is mounted with National Agricultural Research Institute and Chambers of Agriculture (consular organization for farming development) <br>&nbsp;&nbsp;&nbsp;o The call for projects programme is “Innovative initiatives in agriculture and agri-food” launched by French Ministry of Agriculture. <br> - Developing a DST to apply a biocontrol product under optimum conditions <br>&nbsp;&nbsp;&nbsp;o Weenat’s partners are a cooperative, researchers and an agro-chemical industrial <br>&nbsp;&nbsp;&nbsp;o The call for projects programme is the “Single Interdepartmental Fund” housed within the French Ministry of Economy, Industry and Employment.",
+          "key_job_wnt_mission3": "Key account management<span class=\"font-icon-plus\"></span><span class=\"font-icon-minus\"></span>",
+          "key_job_wnt_proj3": "I was responsible of key accounts management by: <br> - Editing quotes, tracking orders and shipments <br> - Assisting installation on site <br> - Caring customers before, during and after the production season <br> - Analysing the feedbacks and transfer it to the person concerned (informatic,electronic, ...) <br> - Brainstorming ways that we might be able to respond to their issues and start newproject if needed",
+          "key_job_wnt_proj4": "I also worked on communication and marketing topics by: <br> - Following the news about agricultural community, technologies, competitors, etc. <br> - Sharing news about the company and agri-tech sector innovations on social networks (twitter, facebook, website) <br> - Participating in fairs and events, like Paris International Agricultural Show (SIA), Paris International Agribusiness Show (SIMA), ... <br> - Contributing to communication media design from being Product Manager",
+          "key_job_wnt_mission4": "Communication and marketing<span class=\"font-icon-plus\"></span><span class=\"font-icon-minus\"></span>",
+
+          "key_job_pio_entr": "Dupont Pioneer",
+          "key_job_pio_desc": "<a target=\"_blank\" href=\"https://www.pioneer.com\">www.pioneer.com</a> | More than 10,000 employees worldwide | Aussonne, FRANCE | Dupont Pioneer is an international seed company specialized in maize, sunflower, oilseed rape and soybean.",
+          "key_job_pio_date1": "May - Dec 2015",
+          "key_job_pio_title1": "Communication and Marketing Assistant",
+          "key_job_pio_text1": "Daily I wrote technical arguments for marketing and communication materials, at the heart of the commercial team. Two other missions:",
+          "key_job_pio_mission1": "Technical Result Book<span class=\"font-icon-plus\"></span><span class=\"font-icon-minus\"></span>",
+          "key_job_pio_proj1": "The results book project is the flagship document of the year. In a first time I was responsible to gathering technical data and testimonies from farmers, structuring it and putting it in words. Then I made the booklet printed in thousands of copies and sent it to French farmers.",
+          "key_job_pio_mission2": "Promotional video <span class=\"font-icon-plus\"></span><span class=\"font-icon-minus\"></span>",
+          "key_job_pio_proj2": "A promotional video about a new opening of seed bags that I put forward, designed, shot and edited has been viewed more than 2,000 times on YouTube.",
+          "key_job_pio_date2": "May - Dec 2014",
+          "key_job_pio_title2": "Agronomist intern",
+          "key_job_pio_mission3": "Corn Disease Decision Support Tool conception <span class=\"font-icon-plus\"></span><span class=\"font-icon-minus\"></span>",
+          "key_job_pio_proj3": "My goal was to bring technical supports to the field agents about the corn crazy top disease (downy mildew): <br> - With documentary researches and field survey, I determined the optimal conditions for the disease <br> - I coded a computerized DST (in VBA) that considers the environmental and <br> - agronomic parameters <br> - I conducted field tests in farms, which have been successful <br>Today the DST is operational and is used by field agents and farmers.",
+
+
+        }
+      },
+      fr: {
+        translation: {
+          "key_title": "Laurie Bésinet",
+          "key_welcome": "Bienvenue",
+          "key_hello": "Je suis actuellement agronome dans une startup alliant nouvelles technologies et agronomie. <br> A la recherche permanente de nouveaux défis, je pars bientôt à la découverte d'un nouveau pays, ses gens, ses cultures et ses agricultures : la Nouvelle-Zélande",
+          "key_newzealand": "Nouvelle-Zélande",
+          "key_resume": "Curriculum Vitae",
+          "key_experiences": "Expériences",
+          "key_projects": "Projets",
+          "key_contact": "Contact",
+          "key_home": "Accueil",
+          "key_job_wnt_entr": "Weenat",
+          "key_job_wnt_desc": "<a target=\"_blank\" href=\"http://www.weenat.com\">www.weenat.com</a> | 5 à 15 employés | Nantes (44), France | Solutions d'agronomie de précision, mobile, connectée et accessible à tous les agriculteurs. Les informations sont collectées de façon ultra-localisée en temps réel par des capteurs, puis analysées et enrichies avec des OAD et enfin présentées dans une interface pratique",
+          "key_job_wnt_date1": "Jan 2016 - Nov 2017",
+          "key_job_wnt_title1": "Chef de projets",
+          "key_job_wnt_text1": "Quatre missions principales:",
+          "key_job_wnt_mission1": "Mission : Création, développement et amélioration de modèles agronomiques et d'outils d'aide à la décision (OAD) <span class=\"font-icon-plus\"></span><span class=\"font-icon-minus\"></span>",
+          "key_job_wnt_proj1": "Rencontre des agriculteurs qui utilisent la plateforme web et les OAD.<br>Gestion des projets avec les différents coeurs de métiers concernés (électronique embarquée, développement informatique, user experience) ou avec d'autres entreprises et organismes.<br>Mise en place d'essais pour valider les OAD installés.",
+          "key_job_wnt_proj2": "",
+          "key_job_wnt_proj3": "",
+          "key_job_wnt_proj4": "",
+        }
+      }
+    }
+  }, function(err, t) {
+    // init set content
+    updateContent();
+  });
+
+  function updateContent() {
+    document.getElementById('i18n_cv_title').innerHTML = i18next.t('key_title');
+//    document.getElementById('i18n_cv_welcome').innerHTML = i18next.t('key_welcome');
+//    document.getElementById('i18n_cv_hello').innerHTML = i18next.t('key_hello');
+    document.getElementById('i18n_cv_newzealand').innerHTML = i18next.t('key_newzealand');
+//    document.getElementById('i18n_cv_newzealand2').innerHTML = i18next.t('key_newzealand');
+    document.getElementById('i18n_cv_resume').innerHTML = i18next.t('key_resume');
+    document.getElementById('i18n_cv_resume2').innerHTML = i18next.t('key_resume');
+    document.getElementById('i18n_cv_experiences').innerHTML = i18next.t('key_experiences');
+//    document.getElementById('i18n_projects').innerHTML = i18next.t('key_projects');
+    document.getElementById('i18n_cv_contact').innerHTML = i18next.t('key_contact');
+//    document.getElementById('i18n_cv_contact2').innerHTML = i18next.t('key_contact');
+    document.getElementById('i18n_cv_home').innerHTML = i18next.t('key_home');
+//    document.getElementByclassname('i18n_cv_job_wnt_title').innerHTML = i18next.t('key_job_wnt_title');
+//    document.getElementById('i18n_cv_job_wnt_date').innerHTML = i18next.t('key_job_wnt_date');
+//    document.getElementById('i18n_cv_job_wnt_desc').innerHTML = i18next.t('key_job_wnt_desc');
+//    document.getElementById('i18n_cv_job_wnt_comp').innerHTML = i18next.t('key_job_wnt_comp');
+//    document.getElementById('i18n_cv_job_wnt_durat').innerHTML = i18next.t('key_job_wnt_durat');
+//    document.getElementById('i18n_cv_job_wnt_plac').innerHTML = i18next.t('key_job_wnt_plac');
+//    document.getElementById('i18n_cv_job_wnt_proj1').innerHTML = i18next.t('key_job_wnt_proj1');
+//    document.getElementById('i18n_cv_').innerHTML = i18next.t('key_');
+//    document.getElementById('i18n_cv_').innerHTML = i18next.t('key_');
+//    document.getElementById('i18n_cv_').innerHTML = i18next.t('key_');
+    for(i =0; i<  document.getElementsByClassName('i18n_cv_job_wnt_mission1').length;i++){document.getElementsByClassName('i18n_cv_job_wnt_mission1')[i].innerHTML= i18next.t('key_job_wnt_mission1');}
+    for(i =0; i<  document.getElementsByClassName('i18n_cv_job_wnt_proj1').length;i++){document.getElementsByClassName('i18n_cv_job_wnt_proj1')[i].innerHTML= i18next.t('key_job_wnt_proj1');}
+    for(i =0; i<  document.getElementsByClassName('i18n_cv_job_wnt_desc').length;i++){document.getElementsByClassName('i18n_cv_job_wnt_desc')[i].innerHTML= i18next.t('key_job_wnt_desc');}
+    for(i =0; i<  document.getElementsByClassName('i18n_cv_job_wnt_entr').length;i++){document.getElementsByClassName('i18n_cv_job_wnt_entr')[i].innerHTML= i18next.t('key_job_wnt_entr');}
+    for(i =0; i<  document.getElementsByClassName('i18n_cv_job_wnt_date1').length;i++){document.getElementsByClassName('i18n_cv_job_wnt_date1')[i].innerHTML= i18next.t('key_job_wnt_date1');}
+    for(i =0; i<  document.getElementsByClassName('i18n_cv_job_wnt_text1').length;i++){document.getElementsByClassName('i18n_cv_job_wnt_text1')[i].innerHTML= i18next.t('key_job_wnt_text1');}
+    for(i =0; i<  document.getElementsByClassName('i18n_cv_job_wnt_title1').length;i++){document.getElementsByClassName('i18n_cv_job_wnt_title1')[i].innerHTML= i18next.t('key_job_wnt_title1');}
+    for(i =0; i<  document.getElementsByClassName('i18n_cv_job_wnt_mission2').length;i++){document.getElementsByClassName('i18n_cv_job_wnt_mission2')[i].innerHTML= i18next.t('key_job_wnt_mission2');}
+    for(i =0; i<  document.getElementsByClassName('i18n_cv_job_wnt_proj2').length;i++){document.getElementsByClassName('i18n_cv_job_wnt_proj2')[i].innerHTML= i18next.t('key_job_wnt_proj2');}
+    for(i =0; i<  document.getElementsByClassName('i18n_cv_job_wnt_mission3').length;i++){document.getElementsByClassName('i18n_cv_job_wnt_mission3')[i].innerHTML= i18next.t('key_job_wnt_mission3');}
+    for(i =0; i<  document.getElementsByClassName('i18n_cv_job_wnt_proj3').length;i++){document.getElementsByClassName('i18n_cv_job_wnt_proj3')[i].innerHTML= i18next.t('key_job_wnt_proj3');}
+    for(i =0; i<  document.getElementsByClassName('i18n_cv_job_wnt_mission4').length;i++){document.getElementsByClassName('i18n_cv_job_wnt_mission4')[i].innerHTML= i18next.t('key_job_wnt_mission4');}
+    for(i =0; i<  document.getElementsByClassName('i18n_cv_job_wnt_proj4').length;i++){document.getElementsByClassName('i18n_cv_job_wnt_proj4')[i].innerHTML= i18next.t('key_job_wnt_proj4');}
+
+    for(i =0; i<  document.getElementsByClassName('i18n_cv_job_pio_mission1').length;i++){document.getElementsByClassName('i18n_cv_job_pio_mission1')[i].innerHTML= i18next.t('key_job_pio_mission1');}
+    for(i =0; i<  document.getElementsByClassName('i18n_cv_job_pio_proj1').length;i++){document.getElementsByClassName('i18n_cv_job_pio_proj1')[i].innerHTML= i18next.t('key_job_pio_proj1');}
+    for(i =0; i<  document.getElementsByClassName('i18n_cv_job_pio_desc').length;i++){document.getElementsByClassName('i18n_cv_job_pio_desc')[i].innerHTML= i18next.t('key_job_pio_desc');}
+    for(i =0; i<  document.getElementsByClassName('i18n_cv_job_pio_entr').length;i++){document.getElementsByClassName('i18n_cv_job_pio_entr')[i].innerHTML= i18next.t('key_job_pio_entr');}
+    for(i =0; i<  document.getElementsByClassName('i18n_cv_job_pio_date1').length;i++){document.getElementsByClassName('i18n_cv_job_pio_date1')[i].innerHTML= i18next.t('key_job_pio_date1');}
+    for(i =0; i<  document.getElementsByClassName('i18n_cv_job_pio_text1').length;i++){document.getElementsByClassName('i18n_cv_job_pio_text1')[i].innerHTML= i18next.t('key_job_pio_text1');}
+    for(i =0; i<  document.getElementsByClassName('i18n_cv_job_pio_mission2').length;i++){document.getElementsByClassName('i18n_cv_job_pio_mission2')[i].innerHTML= i18next.t('key_job_pio_mission2');}
+    for(i =0; i<  document.getElementsByClassName('i18n_cv_job_pio_proj2').length;i++){document.getElementsByClassName('i18n_cv_job_pio_proj2')[i].innerHTML= i18next.t('key_job_pio_proj2');}
+    for(i =0; i<  document.getElementsByClassName('i18n_cv_job_pio_title1').length;i++){document.getElementsByClassName('i18n_cv_job_pio_title1')[i].innerHTML= i18next.t('key_job_pio_title1');}
+    for(i =0; i<  document.getElementsByClassName('i18n_cv_job_pio_mission3').length;i++){document.getElementsByClassName('i18n_cv_job_pio_mission3')[i].innerHTML= i18next.t('key_job_pio_mission3');}
+    for(i =0; i<  document.getElementsByClassName('i18n_cv_job_pio_proj3').length;i++){document.getElementsByClassName('i18n_cv_job_pio_proj3')[i].innerHTML= i18next.t('key_job_pio_proj3');}
+    for(i =0; i<  document.getElementsByClassName('i18n_cv_job_pio_title2').length;i++){document.getElementsByClassName('i18n_cv_job_pio_title2')[i].innerHTML= i18next.t('key_job_pio_title2');}
+    for(i =0; i<  document.getElementsByClassName('i18n_cv_job_pio_date2').length;i++){document.getElementsByClassName('i18n_cv_job_pio_date2')[i].innerHTML= i18next.t('key_job_pio_date2');}
+
+  }
+
+  function changeLng(lng) {
+    i18next.changeLanguage(lng);
+  }
+
+  i18next.on('languageChanged', () => {
+    updateContent();
+  });
+ //test i18next
