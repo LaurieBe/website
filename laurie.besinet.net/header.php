1c94d2d164f2395acad925eb2e1f899e08e1e6de
Lilou <laurie.besinet@gmail.com>
2017-12-31 16:52:16 +1300

    style boutons barre de navigation

--- laurie.besinet.net/header.php
+++ laurie.besinet.net/header.php
@@ -8,18 +8,19 @@
 
 <!-- Homepage Slider -->
 <div id="home-slider">
-    <div class="overlay"></div>
+  <div class="overlay">
+  </div>
 
-    <!--  si ecran < 500 px -->
-    <div class="slider-text" style="top: 50%;left: 50%;transform: translate(-50%, -50%);width: 100%;margin: 0px;height: 200px;">
-      <div id="slidecaption" style="line-height: 100px;">
-      </div>
-    </div>
+  <!--  si ecran < 500 px -->
+  <div class="slider-text" style="top: 50%;left: 50%;transform: translate(-50%, -50%);width: 100%;margin: 0px;height: 200px;">
+    <div id="slidecaption" style="line-height: 100px;"></div>
+  </div>
 
   <div class="control-nav">
-          <a id="nextsection" href="#content"><i class="font-icon-arrow-simple-down"></i></a>
-    </div>
+    <a id="nextsection" href="#goUp"><i class="font-icon-arrow-simple-down"></i></a>
   </div>
+
+</div>
 
 <!-- Header -->
 <header>
@@ -33,13 +34,13 @@
         </a>
         <nav id="menu">
           <ul id="menu-nav">
-            <li><a class="external i18n_home" href="index.php#work"></a></li>
-            <li><a class="external i18n_resume" href="cv.php#resume"></a></li>
-            <li><a class="external i18n_blog" href="blog.php#blog"></a></li>
-            <li><a class="i18n_contact" href="#social-area"></a></li>
+            <li><a class="external i18n_home btn btn-secondary" role="button" href="index.php#work"></a></li>
+            <li><a class="external i18n_resume btn btn-secondary" role="button" href="cv.php#resume"></a></li>
+            <li><a class="external i18n_blog btn btn-secondary" role="button" href="blog.php#blog"></a></li>
+            <li><a class="i18n_contact btn btn-secondary" role="button" href="#social-area"></a></li>
             <li>
-              <button class="button button-mini" onclick="i18next.changeLanguage('en')">EN</button>
-              <button class="button button-mini" onclick="i18next.changeLanguage('fr')">FR</button>
+              <button class="btn btn-outline-light btn-sm" type="button" onclick="i18next.changeLanguage('en')">EN</button>
+              <button class="btn btn-outline-light btn-sm" type="button" onclick="i18next.changeLanguage('fr')">FR</button>
             </li>
           </ul>
         </nav>
